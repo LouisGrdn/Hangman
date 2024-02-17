e4600a094e2a940d30e9a7bfad2aeee6d13108c6
LouisGrdn <130607922+LouisGrdn@users.noreply.github.com>
2024-02-17 21:19:53 +0100

    Adding form to give the word to guess

--- ContentView.swift
+++ ContentView.swift
@@ -8,13 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var game: Game = Game(word: "")
+    @State private var word: String = ""
     var body: some View {
-        VStack {
-            Hangman()
-            AlphabetList().environment(Game(word: "ACEGIKMOQSUWY"))
+        let step = game.step
+        let bindingStep = Binding(get: {step}, set: {newValue in game.step = newValue})
+        if game.word != "" {
+            VStack {
+                HStack {
+                    Spacer()
+                    Button("Recommencer") {
+                        game = Game(word: "")
+                        word = ""
+                    }
+                    .foregroundStyle(.white)
+
+                }
+                Hangman(step: bindingStep)
+                    .frame(width: 300, height: 300)
+                    .padding(.bottom, 50)
+                AlphabetList().environment(game)
+            }
+            .padding()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(.black)
         }
-        .padding()
+        else {
+            TextField("Mot", text: $word)
+                .multilineTextAlignment(.center)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.bottom)
+                .submitLabel(.done)
+            Button("Valider") {
+                if word != "" && word.contains(try! Regex("([a-zA-Z])")) {
+                    game = Game(word: word)
+                }
+                else {
+                    word = ""
+                }
+            }
+        }
     }
+    
 }
 
 #Preview {
